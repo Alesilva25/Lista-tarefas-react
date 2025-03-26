fb9452ea90f75c641603f341779f3a34bb8191e0
Alexandro Silva <Alexandro18silvas@gmail.com>
2025-03-26 16:34:31 -0300

    inicio do projeto

---
+++ lista-tarefas/src/ToDoList.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./ToDoList.css";
+
+function ToDoList() {
+    return (
+        <div>
+            <h1>Lista de Tarefas</h1>
+            <form>
+                <input type="text" placeholder="Adicione uma tarefa" />
+                <button className="add" type="submit">Add</button>
+            </form>
+            <div className="listaTarefas">
+                <div className="item">
+                    <span>Tarefa de exemplo</span>
+                    <button>Deletar</button>
+                </div>
+                <div className="item-completo">
+                    <span>Tarefa de exemplo</span>
+                    <button>Deletar</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ToDoList;
